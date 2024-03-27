01cec63fcf47e8efe8ce31556c37dc028d05ecfe
Sedyankoff <stoil0878@gmail.com>
2024-03-27 14:57:04 +0200

    adjusting resource page

--- resource.js
+++ resource.js
@@ -11,42 +11,47 @@ document.addEventListener('DOMContentLoaded', function() {
     var dropdownContent4 = document.getElementById('dropdownContent4');
 
     dropdownButton1.addEventListener('click', function() {
-        if (dropdownContent.style.display === 'block') {
+        if (dropdownContent.style.display === 'flex') {
             dropdownContent.style.display = 'none';
         } else {
-            dropdownContent.style.display = 'block';
+            dropdownContent.style.display = 'flex';
+            dropdownContent.style.flexDirection = "column";
         }
     });
 
     dropdownButton2.addEventListener('click', function() {
-        if (dropdownContent1.style.display === 'block') {
+        if (dropdownContent1.style.display === 'flex') {
             dropdownContent1.style.display = 'none';
         } else {
-            dropdownContent1.style.display = 'block';
+            dropdownContent1.style.display = 'flex';
+            dropdownContent1.style.flexDirection = "column";
         }
     });
 
     dropdownButton3.addEventListener('click', function() {
-        if (dropdownContent2.style.display === 'block') {
+        if (dropdownContent2.style.display === 'flex') {
             dropdownContent2.style.display = 'none';
         } else {
-            dropdownContent2.style.display = 'block';
+            dropdownContent2.style.display = 'flex';
+            dropdownContent2.style.flexDirection = "column";
         }
     });
 
     dropdownButton4.addEventListener('click', function() {
-        if (dropdownContent3.style.display === 'block') {
+        if (dropdownContent3.style.display === 'flex') {
             dropdownContent3.style.display = 'none';
         } else {
-            dropdownContent3.style.display = 'block';
+            dropdownContent3.style.display = 'flex';
+            dropdownContent3.style.flexDirection = "column";
         }
     });
 
     dropdownButton5.addEventListener('click', function() {
-        if (dropdownContent4.style.display === 'block') {
+        if (dropdownContent4.style.display === 'flex') {
             dropdownContent4.style.display = 'none';
         } else {
-            dropdownContent4.style.display = 'block';
+            dropdownContent4.style.display = 'flex';
+            dropdownContent4.style.flexDirection = "column";
         }
     });
 });
